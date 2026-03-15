acae4efdfa8b26c101fc93be0530650da0d16982
emanuele <vinidandi@gmail.com>
2026-03-15 17:17:55 +0100

    Unified PageLayout with TOP/MIDDLE/BOTTOM banners for all 24000 pages

--- src/components/PageLayout.tsx
+++ src/components/PageLayout.tsx
@@ -1,9 +1,59 @@
 import { ReactNode } from 'react';
+import VideoAdSlot, { VideoBannerSlot } from './VideoAdSlot';
+import NativeAdSlot from './NativeAdSlot';
+import { 
+  Banner728x90, 
+  Banner300x250, 
+  Banner728x90Second,
+  RecommendationWidget,
+  NativeAd,
+  MultiformatAd,
+  MultiformatV2,
+  InstantMessage
+} from './BannerAds';
+import CrackRevenueBanner from './CrackRevenueBanner';
 
 interface PageLayoutProps {
   children: ReactNode;
 }
 
 export default function PageLayout({ children }: PageLayoutProps) {
-  return <>{children}</>;
+  return (
+    <>
+      {/* TOP - After header/hero */}
+      <div className="space-y-2 my-4">
+        <div className="flex justify-center">
+          <Banner728x90 className="hidden md:block" />
+          <Banner300x250 className="md:hidden" />
+        </div>
+        <CrackRevenueBanner />
+        <RecommendationWidget className="my-2" />
+        <NativeAd className="my-2" />
+      </div>
+
+      {/* CONTENT */}
+      {children}
+
+      {/* MIDDLE - Between content sections */}
+      <div className="space-y-2 my-4">
+        <div className="flex justify-center">
+          <Banner728x90Second className="hidden md:block" />
+          <Banner300x250 className="md:hidden" />
+        </div>
+        <CrackRevenueBanner />
+        <MultiformatAd className="my-2" />
+        <MultiformatV2 className="my-2" />
+      </div>
+
+      {/* BOTTOM - Before footer */}
+      <div className="space-y-2 my-4">
+        <div className="flex justify-center">
+          <Banner728x90 className="hidden md:block" />
+          <Banner300x250 className="md:hidden" />
+        </div>
+        <CrackRevenueBanner />
+        <InstantMessage className="my-2" />
+      </div>
+    </>
+  );
 }
